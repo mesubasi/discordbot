efcaa107108d4ca9a2c000150dd4424d62a4199c
mesubasi <mesub08@gmail.com>
2024-10-04 14:29:54 +0300

    migrate typeorm to drizzle

---
+++ src/drizzle/0000_even_timeslip.sql
@@ -0,0 +1,12 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
+/*
+CREATE TABLE IF NOT EXISTS "message" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"username" varchar NOT NULL,
+	"content" varchar NOT NULL,
+	"usercontent" varchar NOT NULL,
+	"createdAt" timestamp with time zone NOT NULL
+);
+
+*/
